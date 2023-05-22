3347c44be7c10c1400373c1ec847a4da5cfe3266
Aryan Singh <114330931+aryanas159@users.noreply.github.com>
2023-05-22 13:56:43 +0530

    locahost address

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -123,7 +123,7 @@ const Chat = () => {
 	};
 
 	const connectToWs = () => {
-		const ws = new WebSocket("ws://localhost:8080");
+		const ws = new WebSocket("ws://localhost:1234");
 		setWs(ws);
 		console.log(ws)
 		ws.addEventListener('open', () => {console.log('opened')})
